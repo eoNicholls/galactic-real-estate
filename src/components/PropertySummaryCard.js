939fe5bddb68d8069fee8efcfce3207740a568a4
Ethan Nicholls <18728509+eoNicholls@users.noreply.github.com>
2020-05-12 19:26:11 +0100

    Style changes

--- src/components/PropertySummaryCard.js
+++ src/components/PropertySummaryCard.js
@@ -5,11 +5,21 @@ import image from '../assets/placeholder-planet.png';
 const PropertySummaryCard = ({ star, planet, size, cost }) => {
   return (
     <div className='summary-card'>
-      <img alt={`${star} ${planet}`} src={image} className='summary-card-image' />
-      <p>
+      <img alt={`${star} ${planet}`} src={image} className='summary-card-image summary-card-child' />
+      <p className='summary-card-child'>
         {star} {planet}.<br />
         {size}.<br />
         £{cost}
+      </p>
+      <p className='summary-card-child summary-card-description'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum.
       </p>
     </div>
   )
