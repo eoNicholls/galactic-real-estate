106781b60ae3f5c02c1da445b1b42cb5386d8681
Ethan Nicholls <18728509+eoNicholls@users.noreply.github.com>
2020-05-14 22:17:32 +0100

    Added a step value to PriceRangeField

--- src/components/PriceRangeField.js
+++ src/components/PriceRangeField.js
@@ -5,9 +5,9 @@ const PriceRangeField = ({props}) => {
   return (
     <div>
       <p>Price from:</p>
-      <input type='number' name='min' placeholder='Min' onChange={props} />
+      <input type='number' name='min' placeholder='Min' step='1000' onChange={props} />
       <p>to:</p>
-      <input type='number' name='max' placeholder='Max' onChange={props} />
+      <input type='number' name='max' placeholder='Max' step='1000' onChange={props} />
     </div>
   )
 }
