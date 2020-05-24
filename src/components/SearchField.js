e86e2853c3aad2196ff33d12b4c6abf11fe2c793
Ethan Nicholls <18728509+eoNicholls@users.noreply.github.com>
2020-05-24 16:00:58 +0100

    Added label to SearchField

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 
 const SearchField = ({ props }) => {
-  return <input
-    type='text'
-    placeholder='Search for keywords'
-    onChange={props}
-  />
+  return (
+    <label> Search for keywords:<br />
+      <input
+        type='text'
+        placeholder='Search'
+        onChange={props} />
+    </label>
+  )
 }
 
 export default SearchField;
